d61ffb8955de308a133867b47060c0d7f529d9f8
tintinhot <tintinhot@gmail.com>
2025-08-14 01:43:50 +0200

    Create next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,28 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  devIndicators: {
+    allowedDevOrigins: [
+        "*.cloudworkstations.dev",
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
