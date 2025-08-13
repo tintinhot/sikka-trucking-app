ab8a80e3239921d7ae03ea1f37087a6d0ae86069
tintinhot <tintinhot@gmail.com>
2025-08-14 01:48:45 +0200

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to Sikka Trucking</h1>
+      <p>Your application is deploying. This page will be updated automatically.</p>
+    </div>
+  );
+}
